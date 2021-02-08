167600537692120dd322c2e792caba879f041ffa
Asfak06 <Asfakmunna@gmail.com>
2021-02-08 19:30:35 +0600

    READ me

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 <p>Story sharing and managing app</p>
 <p>This app was built using Laravel 8</p>
+<p>Please clear the code in boot function in App/AppServiceProvider.php when running the app locally.</p>
 <h2>Bfin-test-project</h2>
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-     $this->app['request']->server->set('HTTPS','on');
+
+      $this->app['request']->server->set('HTTPS','on');
       if(env('APP_ENV')!== 'local'){
         URL::forceScheme('https');
+        
       }
     }
 }
